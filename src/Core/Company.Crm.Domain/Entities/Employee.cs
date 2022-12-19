257f02ad0c7a9d69f783faceb6b73098bc44d30f
Cüneyt Kaya <102078972+cnytkya@users.noreply.github.com>
2022-12-19 23:33:46 +0300

    İlk entitiymizi oluşturduk ve propertylerini tanımlamış olduk

---
+++ src/Core/Company.Crm.Domain/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using Company.Framework.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Crm.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public int UserId { get; set; }
+        public string? IdentityNumber { get; set; }
+        public int? GenderId { get; set; }
+        public int? DepartmentId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public int? StatusTypeId { get; set; }
+        public int? RegionId { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
