0bfd02662d252fffb24aa315f72b8a2da68a8eb7
Cüneyt Kaya <102078972+cnytkya@users.noreply.github.com>
2022-12-19 23:47:31 +0300

    Program.cs (Web.Api) ten direkt SR 'a ulaşması DI ile Company.Crm.Entityframework projesini referans gösterdik. böylelikle gelen istek için artık veritabanından veri çakilebilir.

--- src/Infrastructure/Company.Crm.Entityframework/ServiceRegistrations.cs
+++ src/Infrastructure/Company.Crm.Entityframework/ServiceRegistrations.cs
@@ -10,10 +10,8 @@ namespace Company.Crm.Entityframework
         public static void AddEntityFrameworkRegistration(this IServiceCollection services, IConfiguration configuration)
         {
             //Program.cs ten dogrudan EF e ulaşmaması gerekir fakat biz şimdilik EF i referans alalım.DI ile refere ettik.
-
             var connectionString = configuration.GetConnectionString("Default");
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
         }
-
     }
 }
